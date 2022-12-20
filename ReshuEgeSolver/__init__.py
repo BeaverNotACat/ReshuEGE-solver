ab5d787d683c8514aeb51aaf345ec91d7d4e76a8
beaver <beavernotacat@gmail.com>
2022-12-20 18:57:26 +1000

    Updated settings to yaml

--- ReshuEgeSolver/__init__.py
+++ ReshuEgeSolver/__init__.py
@@ -1,37 +1,49 @@
-import os
 import time
+
+import yaml
+from yaml.loader import SafeLoader
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
-from settings import *
+# from settings import *
 
 
 class ReshuEgeSolver:
-    def __init__(self):
-        self.driver = eval("webdriver."+BROWSER+"()")
+    settings = dict()
+
+    def __init__(self, exam_url, answers_filename):
+        with open("settings.yml", 'r') as stream:
+            self.settings = yaml.load(stream, SafeLoader)
+            print(self.settings)
+
+        with open(answers_filename) as file:
+            self.answers = [line.rstrip() for line in file]
+
+        self.driver = eval("webdriver."+self.settings['BROWSER']+"()")
+        self.exam_url = exam_url
 
     def __log_in(self):
         self.driver.get("https://ege.sdamgia.ru")
 
         email_field = self.driver.find_element(By.ID, "email")
-        email_field.send_keys(EMAIL)
+        email_field.send_keys(self.settings['PROFILE']['EMAIL'])
         password_field = self.driver.find_element(By.ID, "current-password")
-        password_field.send_keys(PASSWORD)
+        password_field.send_keys(self.settings['PROFILE']['PASSWORD'])
         password_field.submit()
         time.sleep(0.4)
 
     def __solve_test(self):
-        self.driver.get(TEST_PAGE_URL)
+        self.driver.get(self.exam_url)
 
         answer_forms = self.driver.find_elements(By.XPATH,
                                                  '//input[not(@type="hidden") and(contains(@class, "test_inp"))]')
-        assert len(answer_forms) == len(TEST_ANSWERS), \
+        assert len(answer_forms) == len(self.answers), \
             "Answers and questions lenthes arent equal"
 
         for form in answer_forms:
-            form.send_keys(TEST_ANSWERS.pop(0))
+            form.send_keys(self.answers.pop(0))
 
         self.driver.find_element(By.XPATH, '//input[contains(@onclick, "submit_form()")]').click()
 
--- run.py
+++ run.py
@@ -1,7 +1,10 @@
+import sys
 from ReshuEgeSolver import ReshuEgeSolver
 
 
-if __name__ == "__main__":
-    solver = ReshuEgeSolver()
+if __name__ == "__main__" and len(sys.argv) == 3:
+    solver = ReshuEgeSolver(sys.argv[1], sys.argv[2])
     solver.run()
+else:
+    print('Please use "python run <test url> <answers filename>"')
     
